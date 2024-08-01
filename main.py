f35e98d390533294c3160c08b68d523cb21e075c
HoucemCHAABANE <houcem.chaabane@aqsone.com>
2024-08-01 10:22:13 +0200

    update 2

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+"This module is test "
 def calculate_x_squared(x):
+  """
+  calcaulate x squared
+  :param x:  int
+  :return: int
+  """
   print("hello world")
   return x*x
 
 def calculate_difference(x, y):
+  """
+  calculate difference
+  :param x: int
+  :param y: int
+  :return: difference int
+  """
   print("Here")
   return y - x
