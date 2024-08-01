2a931658d63a6992346828522388e922205202c1
HoucemCHAABANE <houcem.chaabane@aqsone.com>
2024-08-01 10:47:40 +0200

    update 6

---
+++ tests/test_sample.py
@@ -0,0 +1,15 @@
+"""
+this module for testing
+"""
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, parent_dir)
+from main import calculate_x_squared
+
+def test_squared():
+    """
+    testing squared
+    :return:
+    """
+    assert calculate_x_squared(2) == 4
