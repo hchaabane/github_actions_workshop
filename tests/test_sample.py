559609bd9c00b2d0f0b2cb2c957d1c4766c06bd7
HoucemCHAABANE <houcem.chaabane@aqsone.com>
2024-08-01 10:51:17 +0200

    update 7

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,7 +5,9 @@ import sys
 import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)
+
 from main import calculate_x_squared
+
 
 def test_squared():
     """
